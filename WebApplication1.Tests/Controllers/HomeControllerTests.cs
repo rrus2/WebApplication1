0160e313fad365885de44217e94db499a4a8b2a9
Pavel Ivanko <pavel.ivanko@hotmail.com>
2020-03-03 16:23:48 +0100

    Started unit testing

--- WebApplication1.Tests/Controllers/HomeControllerTests.cs
+++ WebApplication1.Tests/Controllers/HomeControllerTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using WebApplication1.Controllers;
 
 namespace WebApplication1.Tests.Controllers
@@ -14,7 +15,35 @@ namespace WebApplication1.Tests.Controllers
         [TestMethod]
         public void ShouldReturnIndexPage()
         {
+            //arrange
             var controller = new HomeController();
+
+            //act
+            var result = controller.Index();
+
+            //assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldRetunAboutPage()
+        {
+            //assert
+            var controller = new HomeController();
+            //act
+            var result = controller.About();
+            //assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ShouldReturnContactPage()
+        {
+            //arrange
+            var controller = new HomeController();
+            //act
+            var result = controller.Contact();
+            //assert
+            Assert.IsNotNull(result);
         }
     }
 }
---
+++ WebApplication1.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [TestMethod]
+        public void ShouldReturnProductsIndex()
+        {
+            //arrange
+            var products = new List<Product>
+            {
+                new Product { Name = "AK-47", Price = 2000, Stock = 20, GenreID = 1 },
+                new Product { Name = "AK-47", Price = 2000, Stock = 20, GenreID = 1 },
+            };
+            var _productService = new Mock<IProductService>();
+            var _genreService = new Mock<IGenreService>();
+            var _orderService = new Mock<IOrderService>();
+            _productService.Setup(x => x.GetProducts()).Returns(Task.FromResult(products));
+            var controller = new ProductsController(_productService.Object, _genreService.Object, _orderService.Object);
+
+            //act
+            var result = controller.IndexAsync().GetAwaiter().GetResult() as ViewResult;
+            var productsresult = result.Model as List<Product>;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, productsresult.Count);
+        }
+    }
+}
